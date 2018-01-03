416619456c1ef770e75d31df3775b94b8591a572
Kronos35 <xxchronos35xx@gmail.com>
2018-01-02 23:04:58 -0700

    UPDATE

---
+++ autogit.sh
@@ -0,0 +1,2 @@
+#!/bin/bash 
+git add -A;read -rsp $'Press enter to continue...\n';git commit -m "UPDATE";read -rsp $'Press enter to continue...\n';git push;read -rsp $'Press enter to continue...\n';git pull;read -rsp $'Press enter to continue...\n';
